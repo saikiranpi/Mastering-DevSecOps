528c4c5bcdb1687a2531cc918f57fc002f794249
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-10-19 23:44:35 +0530

    Create variable.sh

---
+++ Day 15 AWS-Terraform-Functions-2/variable.sh
@@ -0,0 +1,10 @@
+variable "aws_region" {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "key_name" {}
+variable "azs" {}
+variable "public_cird_block" {}
+variable "private_cird_block" {}
+variable "environment" {}
+variable "ingress_value" {}
+variable "amis" {}
