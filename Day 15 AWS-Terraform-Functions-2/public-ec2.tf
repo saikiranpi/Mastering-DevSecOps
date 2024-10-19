9ac9afd73ac504d063ca50eac92f3800a31c3168
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-10-19 23:41:55 +0530

    Create public-ec2.tf

---
+++ Day 15 AWS-Terraform-Functions-2/public-ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "public-server" {
+  # count = length(var.public_cird_block)
+  count                       = var.environment == "Prod" ? 3 : 1
+  ami                         = lookup(var.amis, var.aws_region)
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = element(aws_subnet.public-subnet.*.id, count.index + 1)
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name        = "${var.vpc_name}-Public-Server-${count.index + 1}"
+    Owner       = local.Owner
+    costcenter  = local.costcenter
+    TeamDL      = local.TeamDL
+    environment = "${var.environment}"
+  }
+
+}
